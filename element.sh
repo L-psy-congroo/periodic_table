0c56361defd51ff72dbb98e13d95e11972e1544a
Glenise Gonzales <gmgonzales@student.hau.edu.ph>
2025-02-10 09:40:36 +0000

    feat: fix code

--- element.sh
+++ element.sh
@@ -1,19 +1,27 @@
 # This script retrieves information about a chemical element from the database.
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
+if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
-  exit 0
+  exit 
 fi
 
-DB_QUERY=$(psql -d periodic_table -t -c "SELECT atomic_number, name, symbol, atomic_mass, type, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties USING (atomic_number) FULL JOIN types USING (type_id) WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1';")
+PSQL="psql --username=postgres --dbname=periodic_table --no-align --tuples-only -c"
 
-if [[ -z "$DB_QUERY" ]]; then
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1;"
+else
+  QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1';"
+fi
+
+RESULT=$($PSQL "$QUERY")
+
+if [[ -z $RESULT ]]; then
   echo "I could not find that element in the database."
-  exit 0
+else
+  echo "$RESULT" | while IFS="|" read -r atomic_number name symbol type atomic_mass melting_point boiling_point; do
+    echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
+  done
 fi
 
-echo "$DB_QUERY" | while IFS="|" read -r atomic_number name symbol atomic_mass type melting_point boiling_point; do
-  echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
-done
 # I just need to add another thing to the commit
